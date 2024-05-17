cc9acb1fc2922939ebc5780a53a6ea04e54f58f8
emanuele-pogliari <manu.pogliari@gmail.com>
2024-05-17 18:09:38 +0200

    main.js modified for vue router

--- src/main.js
+++ src/main.js
@@ -2,8 +2,8 @@ import { createApp } from "vue";
 import "./style.scss";
 import App from "./App.vue";
 import "bootstrap/dist/css/bootstrap.css";
-import "./style.scss";
-createApp(App).mount("#app");
 
 import "bootstrap/dist/js/bootstrap.js";
-import * as bootstrap from 'bootstrap';
+import * as bootstrap from "bootstrap";
+
+createApp(App).use(router).mount("#app");
