9d3f6132cfd8a43a85bfd616a98ab48acc62ed5f
christiandigiandomenico <christian.digiandomenico1990@gmail.com>
2024-05-18 12:21:12 +0200

    feat-humbo fixed

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -15,7 +15,7 @@ export default {
                 
                 <img class="logo-nav" src="/img/booliverylogo.svg" alt="">
 
-                <span>Boolivery - Delivery at your door</span>
+                <span> - Delivery at your door</span>
 
             </div>
 
@@ -51,6 +51,30 @@ export default {
 
     </nav>
 
+    <div class="jumbotron">
+
+        <img class="jumbotron-picture" src="/img/delivery-food.jpg" alt="">
+
+        <div class="title-jumbotron-1">WE DELIVER</div>
+
+        <div class="title-jumbotron-2">AT YOUR DOOR</div>
+
+        <img class="jumbotron-divider" src="/img/divider.png">
+
+        <div class="jumbotron-subsection">But I must explain to you how all this mistaken
+            <br>idea of denouncing of a pleasure
+        </div>
+
+        <div class="buy-jumbotron">
+            
+            <div class="skew-background"></div>
+            
+            <div class="skew-text">GO TO CATALOG</div>
+        
+        </div>
+
+    </div>
+
 </header>
 </template>
 
@@ -60,22 +84,31 @@ export default {
     display: flex;
     justify-content: space-between;
     align-items: center;
-    margin-top: 20px;
-}
+    position: relative;
+    background-color: #f2ca39;
+    padding: 10px;
+    padding-top: 40px;
 
-.left-nav {
-    background-color: rgb(121, 121, 121);
+    .left-nav {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    background-color: white;
     border-radius: 45px;
     padding: 15px 25px;
-}
+    position: absolute;
+    top: 20px;
+    left: 0;
+    z-index: 99;
+    }
 
-.logo-nav {
-    width: 200px;
-}
-
-.middle-nav {
+    .middle-nav {
     display:flex;
     gap: 25px;
+    position: absolute;
+    top: 20px;
+    left: 500px;
+    z-index: 99;
     justify-content: center;
     margin: auto;
     background-color: #3d3d3d;
@@ -83,15 +116,93 @@ export default {
     padding: 20px 25px;
     width: 700px;
     color: white;
-}
+    }
 
-.right-nav {
+    .right-nav {
     background-color: #3d3d3d;
+    position: absolute;
+    top: 20px;
+    left: 1225px;
+    z-index: 99;
     border-radius: 45px;
     padding: 20px 25px;
     color: white;
     width: 400px;
     text-align: center;
+    }
+
+}
+
+.logo-nav {
+    width: 200px;
+}
+
+.jumbotron {
+
+    position: relative;
+
+    img {
+        object-fit: cover;
+        width: 100%;
+        height: 700px;
+    }
+
+    .title-jumbotron-1 {
+        font-size: 70px;
+        position: absolute;
+        top: 175px;
+        left: 100px;
+    }
+
+    .title-jumbotron-2 {
+        font-size: 70px;
+        position: absolute;
+        top: 250px;
+        left: 150px;
+    }
+
+    .jumbotron-divider {
+        width: 250px;
+        height: 30px;
+        position: absolute;
+        z-index: 99;
+        top: 350px;
+        left: 200px;
+        object-fit: cover;
+    }
+
+    .jumbotron-subsection {
+        position: absolute;
+        top: 390px;
+        left: 200px;
+    }
+
+    .buy-jumbotron {
+        position: absolute;
+        width: 250px;
+        text-align: center;
+        top: 470px;
+        left: 250px;
+        .skew-background {
+            background-color: #f2ca39;
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            transform: skewX(20deg);
+            z-index: 1;
+            display: inline-block;
+        }
+        .skew-text {
+            position: relative;
+            z-index: 2;
+            color: white;
+            font-size: 24px;
+            padding: 15px;
+            display: inline-block
+        }
+    }
 }
 
 </style>
