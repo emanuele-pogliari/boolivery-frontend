3233a6f42b8122b0ac56ee45b06f8912bdac60c0
Luca <lucafagiolini@live.it>
2024-05-20 22:42:16 +0200

    nav checkpoint 2, icon fixed, spacing fixed  responsive lg still broken.

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -16,7 +16,7 @@ export default {
                 <!-- <a class="navbar-brand" href="#">BOOLIVERY</a> -->
                 <img class="navbar-brand" src="/img/logo/boolivery-logo-1.png" alt="">
             </div>
-            <div class="d-flex">
+            <div class="d-flex w-100">
                 <div class="nav-links collapse d-flex justify-content-center navbar-collapse accordion rounded-pill"
                     id="navbarSupportedContent">
                     <ul class="navbar-nav mb-lg-0 d-flex justify-content-between align-items-center">
@@ -30,7 +30,7 @@ export default {
                             <a class="nav-link active text-uppercase" aria-current="page" href="#">delivery</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link active text-uppercase" aria-current="page" href="#">about us</a>
+                            <a class="nav-link active text-uppercase" aria-current="page" href="#">about</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link active text-uppercase" aria-current="page" href="#">contact</a>
@@ -38,7 +38,8 @@ export default {
                         <li class="nav-item">
                             <a class="nav-link active text-uppercase" aria-current="page" href="#">payment</a>
                         </li>
-                        <li class="nav-item">
+                        <li
+                            class="shopping-cart-icon d-flex justify-content-center align-items-center nav-item rounded-circle p-1">
                             <i class="nav-link fa-solid fa-cart-shopping"></i>
                         </li>
                     </ul>
@@ -47,7 +48,7 @@ export default {
             <div class="d-flex nav-restaurant-login d-flex justify-content-end rounded-start-pill gap-3">
                 <a class="navbar-brand d-flex gap-2 align-items-center text-uppercase" href="#">
                     <i class="fa-solid fa-utensils"></i>
-                    <span>Restaurant Login</span>
+                    <link rel="stylesheet" href="">Restaurant Login
                 </a>
 
             </div>
@@ -113,20 +114,49 @@ export default {
         .nav-item {
 
             a {
-
                 @include nav_links_style
             }
 
             i {
                 @include nav_links_style
             }
+
+
         }
+
+        .shopping-cart-icon {
+
+            i {
+
+                color: $primary_color !important;
+
+                &:hover {
+                    color: $primary_color !important;
+                }
+
+            }
+
+            background-color: $secondary_color;
+
+
+            &:hover {
+                background-color: $background_color;
+            }
+
+
+
+        }
+
 
     }
 
     .nav-restaurant-login {
         background-color: $primary_color;
         padding: 0.4rem 1.4rem 0.4rem 1.4rem;
+
+        a {
+            @include nav_links_style
+        }
 
     }
 }
