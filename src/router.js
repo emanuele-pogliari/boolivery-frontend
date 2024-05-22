29c433558885fbab4840f663f16219fdac6dd68c
Luca <lucafagiolini@live.it>
2024-05-22 12:27:53 +0200

    Restaraunt page created

--- src/router.js
+++ src/router.js
@@ -1,14 +1,21 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 import HomePage from "./pages/HomePage.vue";
+import RestaurantDetails from "./pages/RestaurantDetails.vue";
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     {
-      path: "/",
+      path: "/home",
       name: "home",
       component: HomePage,
+    },
+
+    {
+      path: "/restaurant",
+      name: "restaurant",
+      component: RestaurantDetails,
     },
   ],
 });
