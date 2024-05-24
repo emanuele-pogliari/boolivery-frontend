04ca3ada23da6ddde13b4d96747aafad347786a8
emanuele-pogliari <manu.pogliari@gmail.com>
2024-05-24 13:12:08 +0200

    checkout page created and braintree drop-in ui added

---
+++ src/Pages/Checkout.vue
@@ -0,0 +1,67 @@
+<script>
+export default {
+  data() {
+    return {};
+  },
+  mounted() {
+    braintree.dropin.create(
+      {
+        authorization: "sandbox_g42y39zw_348pk9cgf3bgyw2b",
+        selector: "#dropin-container",
+      },
+      function (err, instance) {
+        button.addEventListener("click", function () {
+          instance.requestPaymentMethod(function (err, payload) {
+            // Submit payload.nonce to your server
+          });
+        });
+      }
+    );
+  },
+};
+</script>
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-4">
+        <div id="dropin-container"></div>
+        <button id="submit-button" class="button button--small button--green">
+          Purchase
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+<style lang="scss">
+.button {
+  cursor: pointer;
+  font-weight: 500;
+  left: 3px;
+  line-height: inherit;
+  position: relative;
+  text-decoration: none;
+  text-align: center;
+  border-style: solid;
+  border-width: 1px;
+  border-radius: 3px;
+  display: inline-block;
+}
+
+.button--small {
+  padding: 10px 20px;
+  font-size: 0.875rem;
+}
+
+.button--green {
+  outline: none;
+  background-color: #64d18a;
+  border-color: #64d18a;
+  color: white;
+  transition: all 200ms ease;
+}
+
+.button--green:hover {
+  background-color: #8bdda8;
+  color: white;
+}
+</style>
--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 
 import HomePage from "./pages/HomePage.vue";
 import RestaurantDetails from "./pages/RestaurantDetails.vue";
+import Checkout from "./pages/Checkout.vue";
 
 const router = createRouter({
   history: createWebHistory(),
@@ -16,6 +17,11 @@ const router = createRouter({
       path: "/restaurants/:id",
       name: "restaurant",
       component: RestaurantDetails,
+    },
+    {
+      path: "/checkout",
+      name: "checkout",
+      component: Checkout,
     },
   ],
 });
