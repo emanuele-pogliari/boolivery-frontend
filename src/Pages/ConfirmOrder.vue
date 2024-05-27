090669e3a4d5cd9c7309d981454b6f829a3da52a
Luca <lucafagiolini@live.it>
2024-05-27 23:43:40 +0200

    content list

--- src/Pages/ConfirmOrder.vue
+++ src/Pages/ConfirmOrder.vue
@@ -1,5 +1,24 @@
-<script></script>
+<script>
+</script>
+
+
 <template>
-  <h1></h1>
+  <span>"email"</span>
+  <h1>BRAVO "your name" CHE HAI SGANCIATO I SOLDI</h1>
+  <h2>Il tuo ordine è stato confermato</h2>
+
+  <p>find below your recipt from "store"</p>
+
+  <h2>Products</h2>
+  <ul>
+    <li>
+      <p>Product name</p>
+      <p>Product price</p>
+    </li>
+  </ul>
+
+  <h2>total</h2>
+
 </template>
+
 <style></style>
