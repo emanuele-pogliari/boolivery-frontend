a5013c1c48bc7e8055e4e3f634340fac6351a9d3
christiansaccani <christian.saccani99@gmail.com>
2024-05-27 17:59:49 +0200

    card changes

--- src/components/AppCardItem.vue
+++ src/components/AppCardItem.vue
@@ -14,26 +14,36 @@ export default {
 </script>
 
 <template>
-  <div class="container card my_card py-3 gap-2" style="width: 15.5rem">
-    <img
-      v-if="restaurant.image"
-      :src="
-        restaurant.image.startsWith('http')
-          ? restaurant.image
-          : 'http://localhost:8000/storage/' + restaurant.image
-      "
-      class="card-img-top"
-      alt="..."
-    />
-    <img
-      v-else
-      class="card-img-top"
-      src="/img/homepage/placeholdertemp.jpg"
-      alt="..."
-    />
+  <div class="container card my_card pt-3 gap-1">
+    <div class="img-container">
+      <img
+        v-if="restaurant.image"
+        :src="
+          restaurant.image.startsWith('http')
+            ? restaurant.image
+            : 'http://localhost:8000/storage/' + restaurant.image
+        "
+        class="card-img-top"
+        alt="..."
+      />
+      <img
+        v-else
+        class="card-img-top"
+        src="/img/homepage/placeholdertemp.jpg"
+        alt="..."
+      />
+      <div class="likes-pill">
+        <i class="fa-solid fa-thumbs-up"></i> 90%
+      </div>
+    </div>
 
     <div class="card-body">
-      <h5 class="card-title text-center">{{ restaurant.name }}</h5>
+      <h5 class="card-title text-start">{{ restaurant.name }}</h5>
+
+      <p class="card-text text-start" style="font-weight: 500">
+        {{ restaurant.address }}
+      </p>
+
       <div
         class="card-text restaurant_type d-flex gap-1 justify-content-center"
       >
@@ -42,15 +52,6 @@ export default {
         </span>
       </div>
 
-      <div class="d-flex gap-1 justify-content-center mb-2">
-        <i class="fa-solid fa-user"></i><i class="fa-solid fa-user"></i
-        ><i class="fa-solid fa-user"></i><i class="fa-regular fa-user"></i
-        ><i class="fa-regular fa-user"></i>
-      </div>
-
-      <p class="card-text text-center" style="font-weight: 500">
-        {{ restaurant.address }}
-      </p>
       <router-link
         :to="{ name: 'restaurant', params: { id: restaurant.id } }"
         class="btn btn-primary my_btn"
@@ -69,16 +70,39 @@ export default {
   border-color: transparent;
   color: $text_color;
 
+  width: 250px;
+
   box-shadow: 10px 10px 15px rgba(20, 20, 20, 0.1);
   background-color: $background_color;
 
   cursor: default;
 
-  img {
-    border-radius: 16px;
-    height: 10rem;
-    object-fit: cover;
+  .img-container {
+
+    position: relative;
+
+    img {
+      border-radius: 16px;
+      object-fit: cover;
+    }
+
+    .likes-pill {
+      font-weight: 500;
+
+      position: absolute;
+      bottom: 10px;
+      right: 10px;
+
+      background-color: $secondary_color;
+      color: $text_color;
+      padding: 0.1rem 0.5em;
+
+      border-radius: 12px;
+      text-align: center;
+    }
+
   }
+  
 
   p {
     margin: 0.1rem 0.1rem 0.25rem 0.1rem;
@@ -90,13 +114,14 @@ export default {
   .restaurant_type {
     display: flex;
     justify-content: center;
-    margin-bottom: 1rem;
+    margin-bottom: 1.5rem;
+    margin-top: 0.5rem;
 
     span {
-      display: inline-block;
+      font-weight: 500;
 
-      color: $primary_color;
-      background-color: $secondary_color;
+      color: $background_color;
+      background-color: $primary_color;
 
       padding: 0.1rem 0.6rem;
       border-radius: 16px;
@@ -120,7 +145,7 @@ export default {
     padding: 0.7rem 0.9rem;
 
     transition: all 0.2s linear;
-    background: linear-gradient(to bottom right, #f2ca39, #b19019);
+    background: linear-gradient(to bottom right, #f2ca39, #eeba00);
     color: $background_color;
     border: none;
 
