53e9b53c555f4f644225da9fbbc10495efa4b88c
Luca <lucafagiolini@live.it>
2024-05-27 16:52:44 +0200

    fotter restyling mobile-L tablet - desktop responsive

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -11,122 +11,162 @@ export default {
 </script>
 
 <template>
-  <footer class="text-center p-0">
+  <footer class="p-0">
     <!-- <hr class=""> -->
-    <div class="container d-flex justify-content-around gap-5 pt-5 pb-5 px-3">
-      <div class="d-flex flex-column align-items-start gap-1">
-        <!-- <img src="\img\logo\Logo.png" alt=""> -->
-        <h5 class="pt-2">Boolivery</h5>
-        <a href="#" class="btn primary-btn">ORDER NOW</a>
-      </div>
+    <div class="container d-flex py-5">
 
-      <div class="footer_links d-flex flex-column flex-wrap gap-4">
-        <h6>Home</h6>
-        <h6>Delivery</h6>
-        <h6>Payment</h6>
-        <h6>Contact</h6>
-        <h6>About Us</h6>
-      </div>
+      <div class="row d-flex flex-grow-1 gap-5 gap-sm-0 gap-md-0 ">
 
-      <div class="footer_links d-flex flex-column flex-wrap gap-4">
-        <h6>FAQ</h6>
-        <h6>Help Center</h6>
-        <h6>Privacy Policy</h6>
-        <h6>Terms of Service</h6>
-      </div>
-
-      <div class="d-flex flex-column align-items-lg-start gap-2">
-        <div>
-          <h5>Subscribe</h5>
-        </div>
-        <form>
-          <div class="d-flex flex-column align-items-lg-start b-3">
-            <label for="exampleInputEmail1" class="form-label"></label>
-            <input
-              type="email"
-              class="form-control mb-2"
-              id="exampleInputEmail1"
-              placeholder="enter email adress"
-              aria-describedby="emailHelp"
-            />
-            <div id="emailHelp" class="my_text">
-              Subscribe to our newsletter
-            </div>
+        <div
+          class="footer_links d-flex align-items-center align-items-sm-start col-12 col-sm-6 col-md-3 flex-column py-3">
+          <h5>Company</h5>
+          <div>
+            <span>Home</span>
           </div>
-        </form>
-
-        <div>
-          <h5 class="mt-5">Our Socials</h5>
+          <div>
+            <span>Delivery</span>
+          </div>
+          <div>
+            <span>Payment</span>
+          </div>
+          <div>
+            <span>Contact</span>
+          </div>
+          <div>
+            <span>About Us</span>
+          </div>
         </div>
 
         <div
-          class="social_media_contacts d-flex gap-3 flex-wrap justify-content-center"
-        >
-          <span>
-            <a href="">
-              <i class="fa-brands fa-twitter"></i>
-            </a>
-          </span>
-          <span>
-            <a href="">
-              <i class="fa-brands fa-facebook"></i>
-            </a>
-          </span>
-          <span>
-            <a href="">
-              <i class="fa-brands fa-instagram"></i>
-            </a>
-          </span>
-          <span>
-            <a href="">
-              <i class="fa-brands fa-google-plus-g"></i>
-            </a>
-          </span>
-          <span>
-            <a href="">
-              <i class="fa-brands fa-linkedin"></i>
-            </a>
-          </span>
+          class="footer_links align-items-center align-items-sm-start d-flex col-12 col-sm-6 col-md-3 flex-column py-3">
+          <h5>Legal Terms</h5>
+          <div>
+            <span>FAQ</span>
+          </div>
+          <div>
+            <span>Help Center</span>
+          </div>
+          <div>
+            <span>PrivacyPolicy</span>
+          </div>
+          <div>
+            <span>Terms of Service</span>
+          </div>
         </div>
+
+
+        <div
+          class="footer_links social_media_contacts align-items-center align-items-sm-start d-flex col-12 col-sm-6 col-md-3 flex-column py-3">
+          <h5 class="">Our Socials</h5>
+
+          <div class="d-flex gap-2">
+            <span>
+              <a href="">
+                <i class="fa-brands fa-twitter"></i>
+              </a>
+            </span>
+            <span>
+              <a href="">
+                <i class="fa-brands fa-facebook"></i>
+              </a>
+            </span>
+            <span>
+              <a href="">
+                <i class="fa-brands fa-instagram"></i>
+              </a>
+            </span>
+            <span>
+              <a href="">
+                <i class="fa-brands fa-google-plus-g"></i>
+              </a>
+            </span>
+            <span>
+              <a href="">
+                <i class="fa-brands fa-linkedin"></i>
+              </a>
+            </span>
+          </div>
+
+
+
+        </div>
+
+        <div class="d-flex align-items-center align-items-sm-start col-12 col-sm-6 col-md-3 flex-column py-3">
+
+          <div>
+            <h5>Subscribe</h5>
+          </div>
+
+          <form>
+            <div class="d-flex flex-column">
+              <label for="exampleInputEmail1" class="form-label"></label>
+              <input type="email" class="form-control mb-2" id="exampleInputEmail1" placeholder="enter email adress"
+                aria-describedby="emailHelp" />
+              <div id="emailHelp" class="my_text">
+                Subscribe to our newsletter
+              </div>
+            </div>
+          </form>
+
+        </div>
+
+        <!-- <div class="d-flex flex-column align-items-start gap-1">
+        
+        <h5 class="pt-2">Boolivery</h5>
+        <a href="#" class="btn primary-btn">ORDER NOW</a>
+      </div> -->
+
+
+
+
       </div>
 
-      <div></div>
+
     </div>
 
-    <div class="d-flex justify-content-center pb-5 pt-5 pb-5 developers-pills">
-      <div class="d-flex justify-content-around my_width">
-        <div>Website created by</div>
-        <span class="badge rounded-pill">
-          <a href="https://github.com/lucafagiolini">Luca F.</a>
-        </span>
-        <span class="badge rounded-pill">
-          <a href="">Christian DG.</a>
-        </span>
-        <span class="badge rounded-pill">
-          <a href="https://github.com/emanuele-pogliari">Emanuele P.</a>
-        </span>
-        <span class="badge rounded-pill">
-          <a href="">Christian S.</a>
-        </span>
+    <div class="d-flex justify-content-center py-4 developers-pills">
+
+      <div class="d-flex justify-content-between flex-column align-items-center">
+        <span>Website created by</span>
+
+        <!-- <div class="d-flex flex-column gap-3">
+          <span class="badge ">
+            <a href="https://github.com/lucafagiolini">Luca F.</a>
+          </span>
+          <span class="badge">
+            <a href="">Christian DG.</a>
+          </span>
+          <span class="badge">
+            <a href="https://github.com/emanuele-pogliari">Emanuele P.</a>
+          </span>
+          <span class="badge">
+            <a href="">Christian S.</a>
+          </span>
+        </div> -->
+
       </div>
     </div>
 
-    <div class="d-flex justify-content-center p-4">Boolean class 118</div>
+    <div class="d-flex justify-content-center text-white p-4">Boolean class 118</div>
+
   </footer>
 </template>
 
-<style lang="scss">
+<style lang="scss" scoped>
 @use "/src/variabiles.scss" as *;
 @use "/src/mixins.scss" as *;
 
 footer {
   color: $deactivated_text;
   background-color: $primary_color;
+  border-top-right-radius: 100px;
 
-  .container {
-    max-width: 1200px;
-    width: 100%;
+  h5 {
+    font-size: 1.2rem;
+    font-weight: 500;
+    color: $background_color;
   }
+
 
   img {
     height: 100px;
@@ -141,8 +181,12 @@ footer {
   }
 
   .footer_links {
-    h6 {
-      font-size: 1rem;
+
+
+    span {
+
+      font-size: 0.8rem;
+      font-weight: 300;
       color: $deactivated_text;
       cursor: pointer;
 
@@ -169,17 +213,16 @@ footer {
 
   .developers-pills {
     background-color: $secondary_color;
-    font-size: 1rem;
-    font-weight: 500;
+    font-size: 0.8rem;
+    font-weight: 300;
     color: $primary_color;
 
     span {
-      width: 150px;
 
       a {
-        text-decoration: none;
-        @include footer_button_style_pills;
-        cursor: default;
+        // text-decoration: none;
+        // @include footer_button_style_pills;
+        // cursor: default;
 
         &:hover {
           background-color: $secondary_color;
@@ -187,11 +230,6 @@ footer {
       }
     }
   }
-}
-
-.my_width {
-  max-width: 1200px;
-  width: 100%;
 }
 
 .my_text {
