208d8b7771dbeb150dcc8d390442d7c6ac546fc2
emanuele-pogliari <manu.pogliari@gmail.com>
2024-05-31 12:37:42 +0200

    implement modal

--- src/Pages/RestaurantDetails.vue
+++ src/Pages/RestaurantDetails.vue
@@ -21,6 +21,7 @@ export default {
       restaurantsId: null,
       baseApiUrl: "http://127.0.0.1:8000/api/",
       store,
+      // showModal: false,
     };
   },
 
@@ -55,6 +56,7 @@ export default {
         this.store.items.length > 0 &&
         this.store.items[0].restaurantsId !== item.restaurant_id
       ) {
+        // this.openModal();
         alert(
           "Cart contains dishes from another restaurant, please, remove all dishes from cart before continue."
         );
@@ -90,6 +92,10 @@ export default {
       if (cartItem) {
         decreaseItem(cartItem, this.store);
       }
+    },
+
+    openModal() {
+      // this.showModal = true;
     },
 
     updateTotalCartPrice() {
@@ -268,6 +274,34 @@ export default {
       <!-- END CART -->
     </div>
   </div>
+  <!-- <div v-if="showModal" class="modal" tabindex="-1">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Modal title</h5>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div class="modal-body">
+          <p>Modal body text goes here.</p>
+        </div>
+        <div class="modal-footer">
+          <button
+            type="button"
+            class="btn btn-secondary"
+            data-bs-dismiss="modal"
+          >
+            Close
+          </button>
+          <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
+      </div>
+    </div>
+  </div> -->
 </template>
 
 <style lang="scss" scoped>
