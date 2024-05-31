22e2dfa2cbed1294a6115ee4ce3f09bdde717d0b
Luca <lucafagiolini@live.it>
2024-05-31 22:32:24 +0200

    type_badge fixed

--- src/components/AppCardItem.vue
+++ src/components/AppCardItem.vue
@@ -15,27 +15,36 @@ export default {
 
 <template>
   <router-link :to="{ name: 'restaurant', params: { id: restaurant.id } }" class="card my_card p-0">
-    <div class="img-container">
-      <img v-if="restaurant.image" :src="restaurant.image.startsWith('http')
-        ? restaurant.image
-        : 'http://localhost:8000/storage/' + restaurant.image
-        " class="card-img-top" alt="..." />
-      <img v-else class="card-img-top" src="/img/homepage/placeholdertemp.jpg" alt="..." />
-      <div class="types-pill d-flex">
-        <div class="types-single-pill" v-for="type in restaurant.types">
-          <i class="fa-solid fa-pizza-slice"></i> {{ type.type }}
+    <div class="nunito-card">
+
+      <div class="img-container">
+        <img v-if="restaurant.image" :src="restaurant.image.startsWith('http')
+          ? restaurant.image
+          : 'http://localhost:8000/storage/' + restaurant.image
+          " class="card-img-top" alt="..." />
+        <img v-else class="card-img-top" src="/img/homepage/placeholdertemp.jpg" alt="..." />
+
+
+        <div class="types-pill d-flex">
+          <div class="types-single-pill" v-for="type in restaurant.types">
+            <div class="type_bg">
+              <span class="type_text">
+                {{ type.type }}
+              </span>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
 
-    <div class="card-body">
-      <h5 class="card-title text-start">{{ restaurant.name }}</h5>
+      <div class="card-body px-1 py-2">
+        <h5 class="card-title text-start">{{ restaurant.name }}</h5>
 
-      <p class="m-0"><i class="fa-solid fa-thumbs-up"></i> 90%</p>
+        <!-- <p class="m-0"><i class="fa-solid fa-thumbs-up"></i> 90%</p> -->
 
-      <btn class="btn btn-primary my_btn">
-        <i class="fa-solid fa-person-biking"></i>
-      </btn>
+        <!-- <btn class="btn btn-primary my_btn">
+          <i class="fa-solid fa-person-biking"></i>
+        </btn> -->
+      </div>
     </div>
   </router-link>
 </template>
@@ -43,6 +52,12 @@ export default {
 <style lang="scss" scoped>
 @use "/src/variabiles.scss" as *;
 @use "/src/mixins.scss" as *;
+
+.nunito-card {
+  font-family: "Nunito", sans-serif;
+  font-optical-sizing: auto;
+  font-style: normal;
+}
 
 .my_card {
   border-radius: 24px;
@@ -57,8 +72,19 @@ export default {
   cursor: pointer;
   text-decoration: none;
 
+
+  @media screen and (min-width: 768px) {
+    max-width: 325px;
+
+  }
+
   @media screen and (max-width: 768px) {
     max-width: 100%;
+
+  }
+
+  @media screen and (min-width: 1200px) {
+    max-width: 250px;
 
   }
 
@@ -70,30 +96,32 @@ export default {
       border-radius: 16px;
       object-fit: cover;
 
-      height: 10rem;
+      height: 125px;
 
       box-shadow: 10px 10px 15px rgba(20, 20, 20, 0.15);
     }
 
     .types-pill {
-      font-weight: 500;
+      font-weight: 700;
       gap: .1rem;
 
       position: absolute;
       bottom: 10px;
       left: 10px;
 
-      .types-single-pill {
-        background-color: $secondary_color;
-        color: $text_color;
-        border: 1px solid $text_color;
-        padding: 0.1rem 0.5em;
 
-        border-radius: 12px;
-        text-align: center;
+      .type_bg {
+        background-color: $primary_color;
+        opacity: 0.8;
 
-        opacity: .85;
+        .type_text {
+          color: white (1);
+        }
       }
+
+      @include type_single_pill;
+      font-size: 0.8;
+      color: #ffffff (1);
     }
 
   }
@@ -119,27 +147,27 @@ export default {
     white-space: nowrap;
     overflow: hidden;
     text-overflow: ellipsis;
-    line-height: 1.8rem;
-
-    text-shadow: 2px 2px 4px rgba(242, 202, 57, 0.3);
+    font-weight: 800;
+    font-size: 1.2rem;
   }
 
-  .my_btn {
-    position: absolute;
-    bottom: 0%;
-    right: 5%;
-    transform: translate(0%, 0%);
+  // .my_btn {
+  //   position: absolute;
+  //   bottom: 0%;
+  //   right: 5%;
+  //   transform: translate(0%, 0%);
 
-    border-radius: 50%;
-    padding: .5rem .7rem;
+  //   border-radius: 50%;
+  //   padding: .5rem .7rem;
 
-    transition: all 0.2s linear;
-    background: linear-gradient(to bottom right, #f2ca39, #eeba00);
-    color: $background_color;
-    border: none;
+  //   transition: all 0.2s linear;
+  //   background: linear-gradient(to bottom right, #f2ca39, #eeba00);
+  //   color: $background_color;
+  //   border: none;
 
-    box-shadow: 10px 10px 15px rgba(20, 20, 20, 0.15);
-  }
+  //   box-shadow: 10px 10px 15px rgba(20, 20, 20, 0.15);
+  // } 
+
 }
 
 .my_card:hover .my_btn {
